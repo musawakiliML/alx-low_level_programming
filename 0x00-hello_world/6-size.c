e95c6705ee4ee3cea9fc356a98692fd61379eddd
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-16 16:55:07 -0700

    fixed error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,13 +12,13 @@ int main(void)
 	char char_type;
 	int int_type;
 	long int long_int_type;
-	long long int long_long_int_type;
+	long long int long_l_int_type;
 	float float_type;
 
 	printf("Size of a char: %d byte(s)\n", (int)sizeof(char_type));
 	printf("Size of an int: %d byte(s)\n", (int)sizeof(int_type));
 	printf("Size of a long int: %d byte(s)\n", (int)sizeof(long_int_type));
-	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long_long_int_type));
+	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long_l_int_type));
 	printf("Size of a float: %d byte(s)\n", (int)sizeof(float_type));
 	
 	return (0);
