a7254318b481e58ca5e07a81469571bb685e69c8
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-16 16:57:08 -0700

    fixed error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,6 +20,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", (int)sizeof(long_int_type));
 	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long_l_int_type));
 	printf("Size of a float: %d byte(s)\n", (int)sizeof(float_type));
-	
 	return (0);
 }
