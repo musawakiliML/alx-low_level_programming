b8d053f47b81aa6cb6daa62adf0d233291dac824
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-16 16:18:50 -0700

    Added 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry piont
+ *
+ * Descrition - Prints string
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	char char_type;
+	int int_type;
+	long int long_int_type;
+	long long int long_long_int_type;
+	float float_type;
+
+	printf("Size of a char: %d byte(s)\n", (int)sizeof(char_type));
+	printf("Size of an int: %d byte(s)\n", (int)sizeof(int_type));
+	printf("Size of a long int: %d byte(s)\n", (int)sizeof(long_int_type));
+	print("Size of a long long int: %d byte(s)\n", (int)sizeof(long_long_int_type));
+	printf("Size of a float: %d byte(s)\n", (int)sizeof(float_type));
+	
+	return (0);
+}
