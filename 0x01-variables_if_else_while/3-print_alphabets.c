9d2b42b33fa05fcaf2e9f0d60c27e5bfe1b45db5
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-17 15:11:51 -0700

    fixed error

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/*
- * Main function
- * Description: print alphabets in lowercase then uppercase
- * return 0 success
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char letter_lower;
