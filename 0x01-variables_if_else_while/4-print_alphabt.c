f1f8945ec7f6dff503b060a30a6bd3c4f809a459
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-17 15:18:55 -0700

    Added 4-print_alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'q' || letter == 'e')
+		{
+			break;
+		}
+		else
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
