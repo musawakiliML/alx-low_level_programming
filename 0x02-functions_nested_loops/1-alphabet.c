ed1a4ea33d52ee7e4c9f585178176b9ee1247f3a
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-18 16:04:47 -0700

    fixed error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_alphabet: a function that prints the alphabets, in lowercase.
+ * print_alphabet: function entry point
+ * Description: a function that prints the alphabets, in lowercase.
  * Return: Always 0.
  */
 void print_alphabet(void)
