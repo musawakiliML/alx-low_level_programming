5a9beccea2f5ed6d5a96a2249783b1fa3580e8b1
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-18 16:24:21 -0700

    fixed errors

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,9 +12,9 @@ void print_alphabet_x10(void)
 	char letter;
 	int counter;
 
-	for(counter = 0; counter < 10; counter++)
+	for (counter = 0; counter < 10; counter++)
 	{
-		for(letter = 'a'; letter <= 'z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
