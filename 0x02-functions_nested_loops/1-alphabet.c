128448cc2a0c7d3340ced33990face458f302476
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-18 15:54:37 -0700

    Fixed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  * Description: a function that prints 10 times the alphabet, in lowercase.
  * Return: Always 0.
  */
-void print_alphabet_10x(void)
+void print_alphabet_x10(void)
 {
 	char letter;
 
