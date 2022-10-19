7ffb42eb9039b54486a16121ba06e83cdc67c303
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-19 14:24:20 -0700

    fixed errors

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,5 +10,12 @@
 
 int print_last_digit(int n)
 {
-	return (n % 10);
+	int last_digit;
+
+	last_digit = (n % 10);
+	if (last_digit < 0)
+	{
+		last_digit = (-1 * last_digit);
+	}
+	return (last_digit);
 }
