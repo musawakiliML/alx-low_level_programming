43c4433ede4e3a147628792620585d1cc590be10
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-19 16:58:41 -0700

    Added 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - function entry point.
+ * Description - function that adds two integers.
+ * @num1: First Input
+ * @num2: Second Input
+ * Return: the results of two numbers.
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
