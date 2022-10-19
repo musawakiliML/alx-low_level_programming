37a3e59ac9f1e83079a7cc344c0da3be356d1473
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-19 14:43:21 -0700

    Added 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - function entry point.
+ * Description - function that prints every minute of the day.
+ * @n: Input.
+ * Return: 0 success.
+ */
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; h < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0);
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
