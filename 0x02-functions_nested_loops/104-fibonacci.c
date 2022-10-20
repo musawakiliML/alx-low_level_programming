513e4190e63be52e4dd9ea46013d70f35dcad902
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-19 19:33:35 -0700

    Added 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+	long int fibonacci_num[98];
+
+	fibonacci_num[0] = 1;
+	fibonacci_num[1] = 2;
+
+	printf("%lu, %lu, ", fibonacci_num[0], fibonacci_num[1]);
+
+	for (num = 2; num < 98; num++)
+	{
+		fibonacci_num[num] = fibonacci_num[num - 1] + fibonacci_num[num - 2];
+		if (num == 97)
+		{
+			printf("%lu\n", fibonacci_num[num]);
+		}
+		else
+		{
+			printf("%lu, ", fibonacci_num[num]);
+		}
+	}
+	return (0);
+}
