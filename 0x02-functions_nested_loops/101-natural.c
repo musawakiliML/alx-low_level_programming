d60cceece53a2f8e0078c3b4836214a43ffdf719
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-19 19:11:03 -0700

    added 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function entry point.
+ * Description - function that prints the sum of all the mutiples of 3 or 5 below 1024.
+ * Return: always 0 success
+ */
+int main(void)
+{
+	int num;
+	int sum;
+
+	sum = 0;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 5) == 0 || (num % 3) == 0)
+		{
+			sum = sum + num;
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
