5f2c754e0e6740207e1816eddb517ecf5369f987
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-19 19:12:18 -0700

    fixed errors

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,7 +3,8 @@
 
 /**
  * main - function entry point.
- * Description - function that prints the sum of all the mutiples of 3 or 5 below 1024.
+ * Description - function that prints the sum of all
+ * the mutiples of 3 or 5 below 1024.
  * Return: always 0 success
  */
 int main(void)
