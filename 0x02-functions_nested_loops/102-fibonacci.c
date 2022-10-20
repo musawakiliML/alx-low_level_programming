d5afbbeb17e273fc00de71e9993dfa26fed2dc0f
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-19 19:22:20 -0700

    fixed error

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,7 +12,7 @@ int main(void)
 	int num;
 	long int fibonacci_num[50];
 
-	fibonacc_numi[0] = 1;
+	fibonacci_num[0] = 1;
 	fibonacci_num[1] = 2;
 
 	printf("%lu, %lu, ", fibonacci_num[0], fibonacci_num[1]);
