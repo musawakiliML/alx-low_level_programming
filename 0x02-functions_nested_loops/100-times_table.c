4ebb4a51e639e60ac7382e8af9dba971f2aeb3f7
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-20 03:03:31 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -43,28 +43,28 @@ void print_times_table(int n)
 
 void formatfunc(int n)
 {
-	if (num <= 9)
+	if (n <= 9)
 	{
 		_putchar(',');
 		_putchar(' ');
 		_putchar(' ');
 		_putchar(' ');
-		_putchar(num + '0');
+		_putchar(n + '0');
 	}
-	else if (num > 9 && num <= 99)
+	else if (n > 9 && n <= 99)
 	{
 		_putchar(',');
 		_putchar(' ');
 		_putchar(' ');
-		_putchar(num / 10 + '0');
-		_putchar(num % 10 + '0');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
 	}
 	else
 	{
 		_putchar(',');
 		_putchar(' ');
-		_putchar(num / 100 + '0');
-		_putchar(num / 10 % 10 + '0');
-		_putchar(num % 10 + '0');
+		_putchar(n / 100 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
 	}
 }
