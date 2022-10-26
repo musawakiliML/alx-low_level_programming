1462e2c670df2c85df5d0c64717cd1379a8bec61
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-25 19:15:19 -0700

    Added 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function entry point
+ * Description - function that returns the length of a string
+ * @s: string input
+ * Return: Length string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+
+	return (len);
+}
