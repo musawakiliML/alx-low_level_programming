968abe716480ba65bd8c93fa8a1876afd53fe967
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-25 17:13:17 -0700

    Added 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - a function that prints line
+ * Description - draw line _ n times
+ */
+void print_line(int n)
+{
+	int c;
+
+	if (n == 0 || n < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (c = 0; c < n; c++)
+		{
+			_putchar('_');
+		}
+	}
+}
