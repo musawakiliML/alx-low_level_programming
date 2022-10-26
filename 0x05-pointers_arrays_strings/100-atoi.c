260bdf4fc371ecdc65c2394ebc1061f59f02b54d
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-25 20:52:50 -0700

    Added 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _atoi - function entry point
+ * Description - a function that convert a string to an integer
+ * @s: char input
+ * Return: converted string
+ */
+
+int _atoi(char *s)
+{
+	int len;
+	int a, b;
+
+	a = 1;
+	b = 0;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+		if (*s == -1)
+		{
+			a = a * (-1);
+		}
+		else if (*s >= '0' && * <= '9')
+		{	
+			if (h < 0)
+			{
+				b = (b * 10) + (s[len] - '0');
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+	
+	if (a < 0)
+	{
+		b = (-b);
+	}
+
+	return (b);
+
+}
