9861a2ebd09e4087587e888c0d4e8e9363dc0970
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-25 20:03:07 -0700

    Added 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - Entry point
+ * Description - function that prints half of a string, followed by a new line
+ * @str: string input
+ */
+
+void puts_half(char *str)
+{
+	int len, a, b;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+	}
+
+	if (len % 2 == 0)
+	{
+		a = len / 2;
+	}
+	else
+	{
+		a = (len + 1) / 2;
+	}
+
+	for (b = a; b < len; b++)
+	{
+		_putchar(str([b]);
+	}
+	_putchar('\n');
+}	
