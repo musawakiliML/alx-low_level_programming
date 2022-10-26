1b9f24e4506338a2f5b073baa18132656a75851d
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-10-25 19:42:42 -0700

    Added 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - function entry point
+ * Description - a function that reverses a string.
+ * @s: input
+ */
+
+void rev_string(char *s)
+{
+	int len, a;
+	char c;
+
+	a = 0;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+
+	for (; a < len--; ;)
+	{
+		c = s[a];
+		s[a++] = s[len];
+		s[len] = b;
+	}
+}
