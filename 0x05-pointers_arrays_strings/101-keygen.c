5fc3b6e2495354726aae37aae968275620963cfa
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-11-09 16:22:46 -0800

    Added 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - a program that generates random valid
+  * passwords for the program 101-crackme
+  * Return: (0) Always Success
+  */
+
+int main(void)
+{
+	int sum_number;
+	char c;
+
+	srand(time(NULL));
+
+	for (sum_number = 0; sum_number <= 2645; sum_num += c)
+	{
+		c = rand() % 128;
+		putchar(c);
+	}
+	putchar(2772 - sum_number);
+	
+	return (0);
+}
