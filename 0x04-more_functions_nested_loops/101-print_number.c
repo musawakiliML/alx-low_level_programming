ea055020b476dd6fdf7fa678f3c1c41314257c43
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-11-09 16:08:56 -0800

    Added 101-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - a function to  print an integer
+ * @n : input
+ * Return : 0 Always (Sucess)
+*/
+void print_number(int n)
+{
+	int a;
+
+	if (n < 0)
+	{
+		a = -n;
+		_putchar('-');
+	}
+	else
+	{
+		a = n;
+
+	}
+
+	if (a / 10)
+	{
+		print_number(x / 10);
+	}
+	_putchar((a % 10) + '0');
+}
