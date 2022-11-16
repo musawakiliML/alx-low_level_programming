0453855dcb69bc66ac19451c2a11361c0b47eb75
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-11-15 18:35:02 -0800

    Added 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - a function to changes lowercase
+ * to uppercase
+ * @str: the string input
+ * Return: Uppercase String
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+
+	return (str);
+}
