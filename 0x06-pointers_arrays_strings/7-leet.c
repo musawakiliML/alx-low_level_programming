e1fee604d2f53ee653b5c395907b95aed4147727
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-11-15 19:03:18 -0800

    update

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * leet - function Entry point
+ * Description - a function that encodes a
+ * string into 1337
+ * @a: string input
+ * Return: encoded string value
+ */
+
+char *leet(char *a)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	char subs[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	char str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+
+	for (; s[i]; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == str[j])
+			{
+				s[i] = subs[j];
+			}
+		}
+	}
+
+	return (a);
+}
