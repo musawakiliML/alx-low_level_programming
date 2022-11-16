d44da7dcdd206acb72bb5dcb7be6d4ab4dc07d17
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-11-15 19:10:26 -0800

    update

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -18,13 +18,13 @@ char *leet(char *a)
 	char subs[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 	char str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 
-	for (; s[i] != '\0'; i++)
+	for (; a[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (s[i] == str[j])
+			if (a[i] == str[j])
 			{
-				s[i] = subs[j];
+				a[i] = subs[j];
 			}
 		}
 	}
