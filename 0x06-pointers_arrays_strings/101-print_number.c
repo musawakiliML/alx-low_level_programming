4e9c021adcb13619b0d43017af6cb52360262919
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-11-18 01:33:20 -0800

    Added 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_number - funnction entry point 
+ * Description - Print an integer using only _putchar
+ * @n: integer value
+ */
+
+void print_number(int n)
+{
+	unsigned int i;
+	
+	i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
