7d3313cbe36c0476311b49ffde93ffb7eeb6efe6
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-11-18 01:13:52 -0800

    Added 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rot13 - function to convert to rot 13
+ * @str: string input
+ * Return: Converted String
+ */
+
+char *rot13(char *s)
+{
+	int c;
+	int x;
+
+	char *letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	c = 0;
+
+	while (s[c] != '\0')
+	{
+		x = 0;
+		while (letters[x] != '\0')
+		{
+			if (s[c] == letters[x])
+			{
+				s[c] = rot13[x];
+				break;
+			}
+			x++;
+		}
+		c++;
+	}
+	return (s);
+}
