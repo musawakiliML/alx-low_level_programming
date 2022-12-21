220cc674f75ae2f19516e1f554fe369d2f4aa8d1
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-12-21 11:54:53 -0800

    fixed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,20 +11,20 @@
 
 int main(int argc, char *argv[])
 {
-    int i, res = 1;
-
-    if (argc != 3)
-    {
-        printf("%s\n", "Error");
-        return (1);
-    }
-    else
-    {
-        for (i = 1; i < argc; i++)
-        {
-            res *= atoi(argv[i]);
-        }
-    printf("%d\n", res);
-    }
-    return (0);
+	int i, res = 1;
+	
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			res *= atoi(argv[i]);
+		}
+		printf("%d\n", res);
+	}
+	return (0);
 }
