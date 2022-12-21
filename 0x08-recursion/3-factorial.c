e1399ef2761cfbfe265ed9798a975f6e0aa9e960
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-12-21 10:17:20 -0800

    Added task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - recursive function
+ * @n: number of factorial
+ * Return: Always factorial of n
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
