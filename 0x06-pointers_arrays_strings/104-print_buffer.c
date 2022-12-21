23d5ba5de1af7b0979c84ac587108b133a1c36db
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-12-21 03:51:13 -0800

    Added 104-printf_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size of the buffer
+ * Return : void
+ */
+
+void print_buffer(char *b, int size)
+{
+	int a, d, c;
+
+	if (size <= 0)
+	{
+		printf("\n");
+	}
+	else
+	{
+		for (a = 0; a < size; a += 10)
+		{
+			printf("%.8x:",a);
+			for (d = a; d < a + 10; d++)
+			{
+				if (d % 2 == 0)
+				{
+					printf(" ");
+				}
+				if (d < size)
+				{
+					printf("%.2x", *(b + d));
+				}
+				else
+				{
+					printf(" ");
+				}
+			}
+			printf(" ");
+			for (c = a; c < a + 10; c++)
+			{
+				if (l >= size)
+				{
+					break;
+				}
+				if (*(b + c) < 32 || *(b + c) > 126)
+				{
+					printf("%c", '.');
+				}
+				else
+				{
+					printf("%c", *(b + c));
+				}
+			}
+			printf("\n");
+		}
+	}
+}
