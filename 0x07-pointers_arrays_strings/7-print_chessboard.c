665b1f8c77cff18ea823a4af80272290c5af1729
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-12-21 09:04:22 -0800

    Added 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_chessboard - prints the chessboard
+* @a: input pointer.
+* Return: no return.
+*/
+void print_chessboard(char (*a)[8])
+{
+	unsigned int k, b;
+	k = 0;
+	b = 0;
+	
+	for (k = 0; k < 64; k++)
+	{
+		if (k % 8 == 0 && k != 0)
+		{
+			b = k;
+			_putchar('\n');
+		}
+		_putchar(a[k / 8][k - b]);
+	}
+	_putchar('\n');
+}
