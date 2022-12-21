678f74943262a0f057ec2a4722be601526829847
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-12-21 08:10:49 -0800

    updated file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,7 @@
 /**
  *_memset - fill a block of memory with a specific value
  *@s: starting address of memory to be filled
- *@b: the desired value
+ *@b: the desired value input
  *@n: number of bytes to be changed
  *
  *Return: changed array with new value for n bytes
